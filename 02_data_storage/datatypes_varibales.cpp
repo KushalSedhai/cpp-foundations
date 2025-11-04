181a2cf0bc7726cab327c8b49454a13b7613a7a4
Kushal <kushalsedhain@gmail.com>
2025-11-04 15:14:46 -0700

    Understanding data storage

---
+++ 02_data_storage/datatypes_varibales.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /*
+    int a = 5; //4 byte is blocked for storage- type of data used in and size allocation, not always 4 bytes
+    char ch ='a'; //ch is the variable name and ch is the variable, size is 1 byte
+    bool b = 1; // boolean data type, uses 1 bit storage
+    float f = 1.2; // float data type, 8 byte
+    double d = 1.23; //double data type, 8 byte 
+
+
+//Variable name shoudn't start with number and it can start with lower or upper case alphabet
+
+    a = 6;
+    int size = sizeof(a); 
+    cout << "The size of a is: " << size << " Bytes" << endl;
+    cout << "The value of a is: " << a <<endl;
+
+    */
+
+   int a = 'a';
+   cout << "the value of a is: " << a << endl;
+   
+}
