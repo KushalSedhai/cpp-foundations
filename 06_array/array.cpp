9365df85f28445473677ff06d80c82b03653c9ec
Kushal <kushalsedhain@gmail.com>
2025-11-11 21:57:13 -0700

    array and fucntions

---
+++ 06_array/array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void Arr(int Dost[], int size){
+
+    for(int i= 0; i < size; i++){
+        cout << "Index" << i << ": " << Dost[i] << endl;
+    }   
+}
+
+int main(){
+
+    int array[3] = {1,2,3};
+    //cout << "1st index is " << array[1] << endl;
+
+    int k[20] = {0,1,2,3,4};
+    /*for ( int i = 0; i < 20; i++ ){
+        cout << "Printing index " << i << " : " << k[i] << endl;
+    } */
+   Arr(array, 3); 
+   Arr(k, 20);
+   
+    
+}
